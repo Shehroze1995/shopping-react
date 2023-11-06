1c7e3b5f0046e6fd0ca9cfc7aa704c0637025562
Shehroze Khan <shehrozeahmad@yahoo.com>
2023-11-06 19:19:47 +0500

    updated

--- src/components/emptyCart/EmptyCart.jsx
+++ src/components/emptyCart/EmptyCart.jsx
@@ -1,8 +1,11 @@
 import { AiOutlineShopping } from "react-icons/ai";
 import { FaAngleLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { showAllProducts } from "../../features/productsSlice/productsSlice";
+import { useDispatch } from "react-redux";
 
 const EmptyCart = () => {
+  const dispatch = useDispatch();
   return (
     <section className="mb-48">
       <div className="w-max m-auto flex flex-col items-center gap-6 my-8">
@@ -12,6 +15,7 @@ const EmptyCart = () => {
       <Link
         className="flex items-center font-extrabold text-xl w-max m-auto"
         to={`/shopping-react/allProducts/`}
+        onClick={() => dispatch(showAllProducts())}
       >
         <FaAngleLeft />
         <p className="border-b-2 py-1 transition-all duration-300 hover:border-b-blue-500">
